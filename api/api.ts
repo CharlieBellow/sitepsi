bc90756c31c3953e2a1d4bce81fc6375b6563033
Charlie Bellow <charliebftm@gmail.com>
2025-07-03 15:37:32 -0300

    refactor: add db jason server

---
+++ api/api.ts
@@ -0,0 +1,11 @@
+const api = "http://localhost:3001/"
+
+export async function getPosts() {
+  const response = await api.get("/posts")
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts")
+  }
+  return response
+}
+
+
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,7 +8,7 @@ export default function Content() {
     <main className="grid grid-cols-1 pt-10 h-full ">
       <Grid title="Blog">
        {/* declarar uma variável e fazer um map aqui.. exibisndo apenas as 6 primeiras ocorrências */}
-        <div className="grid grid-cols-3 w-full justify-start gap-4">
+        <div className="grid grid-cols-3 w-fit justify-center gap-4">
         
 
           <CardBlog title="BlogBlogBlogBlogBlogBlogBlogBlogBlogBlog" description=" Acesse nosso blog e fique por dentro de tudo que acontece no mundo da psicologia psicologia psicologia psicologia psicologia psicologia psicologia psicologia psicologia psicologia.">
