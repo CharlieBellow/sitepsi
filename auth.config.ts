fd603444334833e6c689a531c76ee50a08f8b99c
charlie <charliebftm@gmail.com>
2025-08-03 14:16:05 -0300

    refactor: add more security

--- auth.config.ts
+++ auth.config.ts
@@ -13,17 +13,32 @@ export const authOptions: NextAuthOptions = {
     GitHub({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        params: {
+          // Solicitando apenas o acesso necessário
+          scope: "read:user user:email",
+        },
+      },
     }),
   ],
   callbacks: {
-   
     // O tipo do 'user' será inferido corretamente agora que o módulo é encontrado.
     async signIn({ user }) {
       if (!user.email) return false
       return allowedEmails.includes(user.email)
     },
+    async session({ session, token }) {
+      if (session.user) {
+        // Adicionando o ID do usuário à sessão
+        session.user.id = token.sub!
+      }
+      return session
+    },
   },
   pages: {
     signIn: "/scvd",
+    error: "/auth/error",
   },
+  debug: process.env.NODE_ENV === "development",
+  secret: process.env.NEXTAUTH_SECRET,
 }
